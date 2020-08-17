35551b9d71b6fbba0957815abdff051d126002aa
Karol Sowa <karosowa@student.agh.edu.pl>
2020-08-17 17:43:13 +0200

    more integration tests

--- event-api/tests/integration/event.test.js
+++ event-api/tests/integration/event.test.js
@@ -1,5 +1,5 @@
 import request from "supertest";
-import { CREATED, OK, BAD_REQUEST } from "http-status";
+import { CREATED, OK, BAD_REQUEST, NOT_FOUND } from "http-status";
 import { expect } from "chai";
 import app from "../../src/app";
 import { Event } from "../../src/api/models/event.model";
@@ -12,6 +12,10 @@ describe("Event API", () => {
     email: "test@domain.com",
     eventDate: new Date(1995, 11, 17, 3, 24, 0).toJSON(),
   };
+  // clean whole DB before each test
+  beforeEach(async () => {
+    await Event.deleteMany({});
+  });
 
   describe("GET api/_healtcheck", () => {
     it("should respond with OK 200 status", async () => {
@@ -60,7 +64,7 @@ describe("Event API", () => {
           .send({
             lastName: "test",
             email: "test@test.pl",
-            eventDate: new Date(1995, 11, 17, 3, 24, 0).toJSON()
+            eventDate: new Date(1995, 11, 17, 3, 24, 0).toJSON(),
           })
           .expect(BAD_REQUEST);
       });
@@ -68,11 +72,6 @@ describe("Event API", () => {
   });
 
   describe("GET /api/v1/events/", () => {
-    // clean whole DB before each test
-    beforeEach(async () => {
-      await Event.deleteMany({});
-    });
-
     it("should retrieve empty list when no events are saved", async () => {
       const res = await request(app).get(`/api/v1/events/`).expect(OK);
       expect(res.body).to.be.a("array");
@@ -80,7 +79,7 @@ describe("Event API", () => {
     });
 
     it("should correctly retrieve mapped event from DB", async () => {
-      const event = new Event(NEW_ENTITY);
+      const event = new Event({ ...NEW_ENTITY });
       await event.save();
 
       const res = await request(app).get(`/api/v1/events/`).expect(OK);
@@ -119,17 +118,77 @@ describe("Event API", () => {
 
   describe("GET /api/v1/events/:id", () => {
     it("should correctly retrieve event from DB", async () => {
-      const event = new Event(NEW_ENTITY);
+      const entity = { ...NEW_ENTITY };
+      console.error("entity", entity);
+      const event = new Event({ ...NEW_ENTITY });
       await event.save();
 
-      const res = await request(app)
-        .get(`/api/v1/events/${NEW_ENTITY['_id']}`)
-        .expect(OK);
-      expect(res.body).to.have.property("id", EXISTING_RESOURCE_ID);
+      const res = await request(app).get(`/api/v1/events/${entity["_id"]}`);
+      expect(res.status).to.equal(OK);
+      expect(res.body).to.have.property("id", NEW_ENTITY["_id"]);
       expect(res.body).to.have.property("firstName");
       expect(res.body).to.have.property("lastName");
       expect(res.body).to.have.property("email");
       expect(res.body).to.have.property("eventDate");
+    });
+
+    it("should return 404 when event does not exist in DB", async () => {
+      const res = await request(app).get(
+        "/api/v1/events/5f3957a73f62a60012ae0000"
+      );
+      expect(res.status).to.equal(NOT_FOUND);
+    });
+
+    it("should return 400 when id cannot be casted", async () => {
+      const res = await request(app).get(`/api/v1/events/not-existing-test-id`);
+
+      expect(res.status).to.equal(BAD_REQUEST);
+    });
+  });
+  describe("DELETE /api/v1/events/:id", () => {
+    it("should correctly delete event from DB", async () => {
+      const event = new Event({ ...NEW_ENTITY });
+      await event.save();
+
+      await request(app)
+        .delete(`/api/v1/events/${NEW_ENTITY["_id"]}`)
+        .expect(OK);
+
+      const response = await Event.findById(NEW_ENTITY["_id"]);
+      expect(response).to.be.a("null");
+    });
+  });
+
+  describe("UPDATE /api/v1/events/:id", () => {
+    it("should correctly delete event from DB", async () => {
+      const event = new Event({ ...NEW_ENTITY });
+      await event.save();
+
+      const date = new Date().toJSON();
+
+      const res = await request(app)
+        .put(`/api/v1/events/${NEW_ENTITY["_id"]}`)
+        .send({
+          firstName: "test-updated",
+          lastName: "test-updated",
+          email: "test-updated@test.pl",
+          eventDate: date,
+        })
+        .expect(OK);
+        // check if entity was updated in DB + response body
+      const entityInDB = await Event.findById(NEW_ENTITY["_id"]);
+      expect(entityInDB).to.have.property("firstName", "test-updated");
+      expect(entityInDB).to.have.property("lastName", "test-updated");
+      expect(entityInDB).to.have.property("email", "test-updated@test.pl");
+      expect(entityInDB).to.have.property("eventDate");
+      expect(new Date(entityInDB.eventDate).toJSON()).to.equal(date);
+
+
+      expect(res.body).to.have.property("id", NEW_ENTITY["_id"]);
+      expect(res.body).to.have.property("firstName", "test-updated");
+      expect(res.body).to.have.property("lastName", "test-updated");
+      expect(res.body).to.have.property("email", "test-updated@test.pl");
+      expect(res.body).to.have.property("eventDate", date);
     });
   });
 });
