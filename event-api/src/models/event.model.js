7e8b9f5389a6d87b464566f809c13846c44215a0
sowakarol <ksowa2019@gmail.com>
2021-07-01 23:21:09 +0200

    email validation better regex

--- event-api/src/models/event.model.js
+++ event-api/src/models/event.model.js
@@ -2,7 +2,7 @@ import { Schema, model } from 'mongoose';
 
 // taken from https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 function validateEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re = /\S+@\S+\.\S+/;
   return re.test(email);
 }
 
